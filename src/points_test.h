29216ddba5056f3014ed711ab7d63fd7d006df3a
yogesh.gandhi@unibo.it <80168409+GandhiYogesh@users.noreply.github.com>
2021-03-10 16:00:51 +0000

    Added 3 more Tests for Point<3>

--- src/points_test.h
+++ src/points_test.h
@@ -25,3 +25,23 @@ TEST(Pythagoras, ScalarProduct)
   Point<2> x(3, 4);
   ASSERT_EQ(x * x, 25);
 }
+
+TEST(Pythagoras3, Norm3)
+{
+  Point<3> x(3, 4, 0);
+  ASSERT_EQ(x.norm(), 5);
+}
+
+
+TEST(Pythagoras3, Distance3)
+{
+  Point<3> x(4, 5, 0);
+  Point<3> y(1, 1, 0);
+  ASSERT_EQ(x.distance(y), 5);
+}
+
+TEST(Pythagoras3, ScalarProduct3)
+{
+  Point<3> x(3, 4, 0);
+  ASSERT_EQ(x * x, 25);
+}
